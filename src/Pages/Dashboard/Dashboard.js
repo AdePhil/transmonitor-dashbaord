8813bdda1b97e34484aaa415f8653712ed37b25d
Adebisi Ahmed <adebisi.ahmed10@gmail.com>
2020-02-02 10:38:43 +0100

    dashboard cards

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
 import "./dashboard.scss";
+import Card from "../../Components/Card/index";
 import DashboardLayout from "../../Components/DashboardLayout";
 class Dashboard extends Component {
   state = {};
   render() {
     return (
       <DashboardLayout>
-        <main>Dashboard contents</main>
+        <>
+          <div className="dashboard__cards">
+            <Card label={"Daily Transaction Volume"} value={"2,342"} />
+            <Card label={"Daily Transaction Value"} value={"₦4,000,000"} />
+            <Card label={"Total Transaction Volume"} value={"452,000"} />
+            <Card label={"Total Transaction Value"} value={"₦4,000,000"} />
+          </div>
+        </>
       </DashboardLayout>
     );
   }
